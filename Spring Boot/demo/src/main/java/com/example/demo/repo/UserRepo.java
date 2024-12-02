effe43cd09bf27eff09a48647b5abccacfac71a8
Chanaka Dilshan <chanakad633@gmail.com>
2024-12-02 16:33:37 +0530

    Filter events for user done.

--- Spring Boot/demo/src/main/java/com/example/demo/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.Users;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends MongoRepository<Users, String> {
-}
