f4e9e66586ce7ed334756baae62827b840e4752e
KavishkaAswaththa <157552673+KavishkaAswaththa@users.noreply.github.com>
2024-12-11 20:38:19 +0530

    Delete Spring Boot/demo/src/main/java/com/example/demo/repo/SignUpRepository.java

--- Spring Boot/demo/src/main/java/com/example/demo/repo/SignUpRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SignUpRepository extends MongoRepository<User, String> {
-}
