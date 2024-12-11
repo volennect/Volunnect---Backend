8749b5ac828e70bb8e6a584c1ac974e00184045d
Chanaka Dilshan <chanakad633@gmail.com>
2024-12-12 03:17:55 +0530

    update

--- Spring Boot/demo/src/main/java/com/example/demo/repo/filterUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.FilterUsers;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface filterUserRepo extends MongoRepository<FilterUsers, Long> {
-}
